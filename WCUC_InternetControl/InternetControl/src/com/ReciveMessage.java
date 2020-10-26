e9904b674a949bbf48bbdc257e2232d64ffe8a53
GarFiwld <garfiwld@icloud.com>
2020-10-27 01:00:46 +0700

    WCUC_InternetControl

---
+++ WCUC_InternetControl/InternetControl/src/com/ReciveMessage.java
@@ -0,0 +1,58 @@
+package com;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class ReciveMessage {
+
+    Thread ReciveMsg;
+
+    public void start() {
+
+        ReciveMsg.start();
+
+    }
+
+    public ReciveMessage() {
+        this.ReciveMsg = new Thread(new Runnable() {
+
+            @Override
+            public void run() {
+                System.out.println("ReciveMessage Start");
+                try {
+                    ServerSocket serversocketStudent = new ServerSocket(26104);
+                    while (true) {
+                        Socket readAccept = serversocketStudent.accept();
+                        BufferedReader read = new BufferedReader(new InputStreamReader(readAccept.getInputStream()));
+                        String msg = read.readLine();
+                        String room = read.readLine();
+                        System.out.println("case : InternetOn | room : " + room);
+                        switch (msg) {
+                            case "InternetOn":
+                                try {
+                                    Runtime.getRuntime().exec("C:\\WCUC_InternetControl\\WCUC_Autoit\\" + room + "_UnBlock.exe");
+                                    System.out.println("case : InternetOn | room : " + room + " completed.");
+                                } catch (IOException e) {
+                                    System.out.println("case : InternetOn | room : " + room + " failed.");
+                                }
+                                break;
+                            case "InternetOff":
+                                try {
+                                    Runtime.getRuntime().exec("C:\\WCUC_InternetControl\\WCUC_Autoit\\" + room + "_Block.exe");
+                                    System.out.println("case : InternetOn | room : " + room + " completed.");
+                                } catch (IOException e) {
+                                    System.out.println("case : InternetOn | room : " + room + " failed.");
+                                }
+                                break;
+                        }
+                    }
+                } catch (IOException ex) {
+                    System.out.println(ex);
+                }
+            }
+        });
+    }
+}
---
+++ WCUC_InternetControl/WCUC_Autoit/621_Block.au3
@@ -0,0 +1,51 @@
+#include "wd_core.au3"
+#include "wd_helper.au3"
+#include <MsgBoxConstants.au3>
+
+Local $sDesiredCapabilities, $sSession
+
+SetupGecko()
+
+_WD_Startup()
+
+$sSession = _WD_CreateSession($sDesiredCapabilities)
+_WD_Navigate($sSession, "https://192.168.14.1/")
+_InputValueById($sSession, 'usernamefld', 'admin')
+_InputValueById($sSession, 'passwordfld', 'b4281btx')
+$sButton = _WD_FindElement($sSession, $_WD_LOCATOR_ByXPath, "//input[@name='login']")
+ _WD_ElementAction($sSession,$sButton, 'click')
+
+_WD_Navigate($sSession, 'https://192.168.14.1/firewall_rules_edit.php?id=9')
+
+
+$sElement = _WD_FindElement($sSession, $_WD_LOCATOR_ByXPath, "//input[@id='disabled']")
+$sValue = _WD_ElementAction($sSession, $sElement, 'selected')
+;MsgBox(0, 'result', $sValue)
+If $sValue = True Then
+   _WD_ElementAction($sSession,$sElement, 'click')
+EndIf
+
+
+
+$sButton = _WD_FindElement($sSession, $_WD_LOCATOR_ByXPath, "//button[@id='save']")
+_WD_ElementAction($sSession,$sButton, 'click')
+
+_WD_Navigate($sSession, 'https://192.168.14.1/firewall_rules.php?if=lan')
+$sButton = _WD_FindElement($sSession, $_WD_LOCATOR_ByXPath, "//button[@name='apply']")
+ _WD_ElementAction($sSession,$sButton, 'click')
+
+_WD_DeleteSession($sSession)
+_WD_Shutdown()
+
+Func SetupGecko()
+_WD_Option('Driver', 'geckodriver.exe')
+_WD_Option('DriverParams', '--log trace')
+_WD_Option('Port', 4444)
+
+$sDesiredCapabilities = '{"capabilities": {"alwaysMatch": {"browserName": "firefox", "acceptInsecureCerts":true}}}'
+EndFunc
+
+Func _InputValueById($sSession,$Id,$Value)
+ $sButton = _WD_FindElement($sSession, $_WD_LOCATOR_ByXPath, "//input[@id='"&$Id&"']")
+ _WD_ElementAction($sSession,$sButton,'value', $Value)
+EndFunc
